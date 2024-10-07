95a26a56860d9cec56e6c9fc8159df3600e11ed4
Oswald-Che <cheoswald@yahoo.com>
2024-10-07 16:30:16 +0100

    get photos of user and display photos in the index view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
-
 class StaticPagesController < ApplicationController
   def index
-    
+    flickr = Flickr.new
+    @photos = flickr.photos.search user_id: params[:user_id] if params[:user_id]
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
-require 'rubyems'
-require 'flickr'
+require "flickr"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
